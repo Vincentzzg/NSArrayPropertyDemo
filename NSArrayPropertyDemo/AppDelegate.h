5e89b209bb974f1364f5f4021e2c7219e4eeee17
zhouzhongguang <1137138434@qq.com>
2018-05-07 14:48:17 +0800

    Initial Commit

---
+++ NSArrayPropertyDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NSArrayPropertyDemo
+//
+//  Created by zzg on 2018/5/7.
+//  Copyright © 2018年 zzg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
